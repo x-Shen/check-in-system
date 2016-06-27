1088c453cdbecfa06194f29f599ef514593c1a92
Daniel Cheng <chengd4@uci.edu>
2016-06-27 12:07:33 -0700

    Fixed a bug where checkout info would not load sometimes

--- portalCheckIn/public/controller/adminController.js
+++ portalCheckIn/public/controller/adminController.js
@@ -32,31 +32,31 @@ appLogin.controller("adminController", ['$scope', '$http', '$modal', '$state', '
             $http.get('/admins/viewCheckIn?token=' + CheckInService.getToken()).then(function (res) {
                 console.log(res.data);
                 $scope.checkin = res.data;
-            });
+                $http.get('/admins/viewCheckOut?token=' + CheckInService.getToken()).then(function (res) {
+                    console.log(res.data);
+                    $scope.checkout = res.data;
 
-            $http.get('/admins/viewCheckOut?token=' + CheckInService.getToken()).then(function (res) {
-                console.log(res.data);
-                $scope.checkout = res.data;
+                    //Giving all the checkin objects a checkout time to display on the adminView
+                    for (ci in $scope.checkin){
+                      var time;
+                      var best = null;
+                      for (co in $scope.checkout){
+                        if ($scope.checkin[ci].user.studentId == $scope.checkout[co].user.studentId){
+                          time = new Date($scope.checkout[co].createdAt) - new Date($scope.checkin[ci].createdAt);
 
-                //Giving all the checkin objects a checkout time to display on the adminView
-                for (ci in $scope.checkin){
-                  var time;
-                  var best = null;
-                  for (co in $scope.checkout){
-                    if ($scope.checkin[ci].user.studentId == $scope.checkout[co].user.studentId){
-                      time = new Date($scope.checkout[co].createdAt) - new Date($scope.checkin[ci].createdAt);
-
-                      if (!best && time > 0){
-                        best = time;
-                        $scope.checkin[ci].test = $scope.checkout[co].createdAt;
-                      }else if (best > time && time > 0){
-                        best = time;
-                        $scope.checkin[ci].test = $scope.checkout[co].createdAt;
+                          if (!best && time > 0){
+                            best = time;
+                            $scope.checkin[ci].test = $scope.checkout[co].createdAt;
+                          }else if (best > time && time > 0){
+                            best = time;
+                            $scope.checkin[ci].test = $scope.checkout[co].createdAt;
+                          }
+                        }
                       }
                     }
-                  }
-                }
+                });
             });
+
 
             //get all the user data from the database
 
